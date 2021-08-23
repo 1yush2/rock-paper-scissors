857887f42e559dcd2cb2f871b43733b1d20b39b6
Aayush Karki <1yush225@gmail.com>
2021-08-23 17:33:19 +0545

    Added new fonts,improved header and finished js for game rock paper and scissors

--- rcs.js
+++ rcs.js
@@ -6,33 +6,67 @@ if(nemOk == ""){
 }
 
 const chooseButtons = document.querySelectorAll('[data-works]')
+const finalCol = document.querySelector('[data-after-computer]')
+const compScoreSpan = document.querySelector('[data-comp-score]')
+const myScoreSpan = document.querySelector('[data-my-score]')
 const CHOSE = [
     {
-        name : 'Rocks',
+        name : 'Rock',
         emoji : '👊',
         wins : 'Scissors'
     },
     
     {
-        name : 'Papers',
+        name : 'Paper',
         emoji : '🖐',
-        wins : 'Rocks'
+        wins : 'Rock'
     },    
     
     {
         name : 'Scissors',
         emoji : '✌️',
-        wins : 'Papers'
+        wins : 'Paper'
     }
 ]
 
 chooseButtons.forEach(chooseButton => {
     chooseButton.addEventListener('click', () => {
         const choseName = chooseButton.dataset.works
-        MakesACall(choseName)
+        const chose = CHOSE.find(chose => chose.name === choseName)
+        MakesACall(chose)
     })
 })
 
 function MakesACall(info){
-    console.log(info)
+    computerCHose = randomChoose()
+    
+    ourWinner = didWon(info,computerCHose)
+    oppWinner = didWon(computerCHose,info)
+    
+    chosenResult(computerCHose,oppWinner)
+    chosenResult(info,ourWinner)
+
+    if(ourWinner) increament(myScoreSpan)
+    if(oppWinner) increament(compScoreSpan)
+}
+
+let increament = (scoreSpan) => {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+} 
+
+let chosenResult = (chosen,won) => {
+    const div = document.createElement('div')
+    div.innerText = chosen.emoji
+    div.classList.add('score-select')
+    if (won) div.classList.add ('winner')
+    finalCol.after(div)
+}
+
+let didWon = (ourChoose,oppChoose) => {
+    return ourChoose.wins === oppChoose.name
+}
+
+function randomChoose(){
+    const ranIndex = Math.floor(Math.random() * CHOSE.length)
+    return CHOSE[ranIndex]
 }
